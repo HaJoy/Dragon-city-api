63acf9b40b06fc512de11f29b372e9e525b0352e
HaJoy <hammercanog@gmail.com>
2024-11-29 03:55:01 -0500

    Card images now work

--- frontend/src/components/DragonCard.js
+++ frontend/src/components/DragonCard.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const DragonCard = ({ dragon }) => {
+  const imageName = dragon.name.split(' ').join('_');
   return (
     <div className="card col" style={{width: 18 + 'rem'}}>
-        <img src={dragon.imageURL} className="card-img-top" alt={dragon.name} />
+        <img src={`images/${imageName}.webp`} className="card-img-top" alt={dragon.name} />
         <div className="card-body">
             <h5 className="card-title">{dragon.name}</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
